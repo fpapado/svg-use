d90181101a285b4e342e09e780070b7059eaef60
Fotis Papadogeorgopoulos <fotis.papadogeorgopoulos@wolt.com>
2024-08-15 14:52:22 +0300

    Add external icon demos to examples

--- examples/vite-react/src/App.tsx
+++ examples/vite-react/src/App.tsx
@@ -1,23 +1,19 @@
-import { useState } from 'react';
-import { id, url, viewBox } from './assets/react.svg?svgUse';
 import { ThemedSvg } from '@svg-use/react';
-import viteLogoUrl from '/vite.svg';
-import reactLogoUrl from './assets/react.svg';
+import { id, url, viewBox } from './assets/react.svg?svgUse';
+import {
+  AlertCircle,
+  AlertTriangle,
+  Archive,
+  ArrowRight,
+} from 'shared-library';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogoUrl} className="logo" alt="Vite" />
-        </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogoUrl} className="logo react" alt="React" />
-        </a>
-        <a href="https://react.dev" target="_blank">
+          {/* NOTE: This does not work during development, because Rollup plugins cannot emit in Vite's dev mode */}
           <ThemedSvg
             role="img"
             aria-label="React"
@@ -28,18 +24,11 @@ function App() {
           />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>External icon demos</p>
+      <AlertCircle color="orange" role="img" aria-label="Warning" />
+      <AlertTriangle />
+      <Archive />
+      <ArrowRight />
     </>
   );
 }
