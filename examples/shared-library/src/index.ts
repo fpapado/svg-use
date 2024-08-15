f000ded9fa23bb8be23ae239e346372e49eb42ff
Fotis Papadogeorgopoulos <fotis.papadogeorgopoulos@wolt.com>
2024-08-15 10:33:02 +0300

    Expand shared library example (#8)
    * Expand shared-library example
    
    * Add declaration maps, tidy up CI
    
    * Fix CI conditionals
    
    * Update lockfile
    
    * One more conditional optimisation

--- examples/shared-library/src/index.ts
+++ examples/shared-library/src/index.ts
@@ -1,4 +1,4 @@
-export { Component as AlertCircle } from './icons/AlertCircle.ts';
-export { Component as AlertTriangle } from './icons/AlertTriangle.ts';
-export { Component as Archive } from './icons/Archive.ts';
-export { Component as ArrowRight } from './icons/ArrowRight.ts';
+export { Component as AlertCircle } from './icons/AlertCircle.js';
+export { Component as AlertTriangle } from './icons/AlertTriangle.js';
+export { Component as Archive } from './icons/Archive.js';
+export { Component as ArrowRight } from './icons/ArrowRight.js';
