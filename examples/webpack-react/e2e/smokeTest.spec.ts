ed5840dbe4c3b4f805e805e38224a63840efab0d
Fotis Papadogeorgopoulos <fotis.papadogeorgopoulos@wolt.com>
2024-08-21 15:25:32 +0300

    Add e2e tests to webpack example

---
+++ examples/webpack-react/e2e/smokeTest.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test';
+
+test('it renders all SVGs as expected', async ({ page }) => {
+  await page.goto('/');
+  const images = await page.getByRole('img').all();
+
+  expect.soft(images.length).toBe(8);
+
+  for (const [index, image] of images.entries()) {
+    await expect(image).toHaveScreenshot(`svg-${index}.png`);
+  }
+});
