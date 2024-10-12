27d0deb6e13e0df0ca6234e4223aa2f7c07e0fa4
Fotis Papadogeorgopoulos <fotis.papadogeorgopoulos@wolt.com>
2024-10-12 11:46:11 +0300

    Use URL.parse() as a progressive enhancement in dev-only checks

--- packages/react/src/runtimeChecks.dev.tsx
+++ packages/react/src/runtimeChecks.dev.tsx
@@ -1,17 +1,32 @@
 /** Development-only checks on the supplied parameters */
 export const runtimeChecks = (href: string) => {
-  if (!URL.canParse(href)) {
+  const base = window !== undefined ? window.location.href : undefined;
+  const url = urlParse(href, base);
+
+  if (!url) {
     console.warn(
       `Could not parse href ${href} as URL. The SVG will not be displayed correctly. This might imply a loader misconfiguration in your bundler. Ensure that the SVG that you are trying to reference is loaded via its URL.`,
     );
     return;
   }
 
-  const url = new URL(href, window.location.href);
-
   if (window !== undefined && window.location.origin !== url.origin) {
     console.warn(
       `The SVG you are trying to load is on the ${url.origin} origin, but the current site is ${window.location.origin}. SVG use[href] only works with same-origin URLs, and there is no mechanism to make them available cross-origin. Your options are to either self-host the SVG under your own origin, or to set up a proxy.`,
     );
+  }
+};
+
+const urlParse = (href: string, base?: string): URL | null => {
+  if ('parse' in URL) {
+    //@ts-expect-error TS does not know about URL.parse yet
+    //@see https://developer.mozilla.org/en-US/docs/Web/API/URL/parse_static
+    return URL.parse(href, base) as URL | null;
+  }
+
+  try {
+    return new URL(href, base);
+  } catch {
+    return null;
   }
 };
